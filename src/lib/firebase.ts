33171b9037a4c80794aebfac0cebc01a74ff8862
ShiruvatiNarasimha <mrnattyboy05@gmail.com>
2025-02-19 18:15:39 +0530

    Firebase uploadFile

---
+++ src/lib/firebase.ts
@@ -0,0 +1,67 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDuN9FJtFNo5E6XD-y4xQsE-xem8oufVQE",
+  authDomain: "github-saas-b047c.firebaseapp.com",
+  projectId: "github-saas-b047c",
+  storageBucket: "github-saas-b047c.firebasestorage.app",
+  messagingSenderId: "837268111997",
+  appId: "1:837268111997:web:cf441fc175ffedf387a714",
+  measurementId: "G-BXERP96LF8",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const storage = getStorage(app);
+
+export async function uploadFile(
+  file: File,
+  setProgress?: (progress: number) => void,
+) {
+  return new Promise((resolve, reject) => {
+    try {
+      const storageRef = ref(storage, file.name);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress = Math.round(
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100,
+          );
+          if (setProgress) setProgress(progress);
+          switch (snapshot.state) {
+            case "paused":
+              console.log("upload is paused");
+              break;
+            case "running":
+              console.log("upload is running");
+              break;
+          }
+        },
+        (error) => {
+          reject(error);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadUrl) => {
+            resolve(downloadUrl);
+          });
+        },
+      );
+    } catch (error) {
+      console.error(error);
+      reject(error);
+    }
+  });
+}
